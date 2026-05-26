425c2cecf271c45f3806472ad9ab2f395bd82548
Venkateswara Reddy <113894770+venkyy8@users.noreply.github.com>
2026-05-26 10:41:42 +0530

    Update data_provider.py

--- data/data_provider.py
+++ data/data_provider.py
@@ -3,26 +3,20 @@ import pandas as pd
 
 
 def get_market_data(symbol="RELIANCE.NS", interval="5m", period="5d"):
-    """
-    Fetch intraday market data (FREE)
-    """
-
-    print(f"📡 Fetching live data for {symbol}...")
 
     df = yf.download(
         tickers=symbol,
         interval=interval,
         period=period,
-        progress=False
+        progress=False,
+        threads=False
     )
 
-    if df.empty:
-        print("❌ No data received")
+    if df is None or df.empty:
         return None
 
     df = df.reset_index()
 
-    # Standardize column names for your pipeline
     df.rename(columns={
         "Datetime": "timestamp",
         "Open": "open",
@@ -31,7 +25,5 @@ def get_market_data(symbol="RELIANCE.NS", interval="5m", period="5d"):
         "Close": "close",
         "Volume": "volume"
     }, inplace=True)
-
-    print("✅ Data fetched successfully")
 
     return df
